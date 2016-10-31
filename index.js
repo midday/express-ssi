aa4c51bdcab0dd5079d007f18c4cbd254c52c458
liuzhengwu(刘正午.信息技术中心.EC开发部) <liuzhengwu@ds.gome.com.cn>
2016-10-31 17:57:32 +0800

    modify ssi request fail log

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ function replaceContentBySSITagParse(ssiServer, body) {
         var path = ssiTag.match(/"(.+?)"/gim)[0].replace(/"/g, '');
         var ssiResponse;
 
-        try{
+        try {
             ssiResponse = request('GET', ssiServer + path).getBody().toString();
-        }catch(e){
-            console.log('[express ssi]:fetch ssi request failed! pleace check ssi tag:'+ssiTag);
+        } catch (e) {
+            console.log("错误:有一个ssi请求失败，可通过如下两步进行排查：");
+            console.log("  (1)检查ssi标签有无语法错误：" + ssiTag);
+            console.log("  (2)检查ssi请求能否正常访问：" + ssiServer + path);
             ssiResponse = ssiTag;
         }
 
